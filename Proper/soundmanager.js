14968496abc77c126b9806f688209351226d18df
Preceti <161339561+Preceti@users.noreply.github.com>
2024-03-04 09:16:47 +0100

    Add files via upload

---
+++ Proper/soundmanager.js
@@ -0,0 +1,47 @@
+// no sound by default, need be read before script
+var ambiantsound ="off";
+// Fulgora sound file list
+const FulgoraThunderfiles = [
+    "sound/Thunder.mp3"  
+  ];
+
+const FulgoraWindfiles=[
+    "sound/Wind.mp3"
+];
+
+
+  
+  // Fulgora array storing each sound as audio
+  let FulgoraThunder = [];
+  FulgoraThunderfiles.forEach((element) => {
+    FulgoraThunder.push(new Audio(element));
+  });
+  let FulgoraWind = [];
+  FulgoraWindfiles.forEach((element) => {
+    FulgoraWind.push(new Audio(element));
+  });
+
+
+
+function customrepeatplay(targetArray) {
+    if (!targetArray) {
+      return;
+    }
+    let target = targetArray.find((sound) => !sound.busy);
+    if (!target) {
+      return;
+    }
+  
+    target.busy = true;
+    target.source = targetArray;
+    target.addEventListener("ended", setCanplay, false);
+    target.play();
+  }
+
+  function setCanplay(event) {
+    event.target.removeEventListener("ended", setCanplay, false);
+    event.target.busy = false;
+    if( ambiantsound= "playing"){
+   customrepeatplay(event.target.source)}
+  }
+  
